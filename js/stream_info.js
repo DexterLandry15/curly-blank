a6102501e22218740baa2e9a65108ddb94b55a0b
DexterLandry15 <56548223+DexterLandry15@users.noreply.github.com>
2022-10-06 18:31:08 +0300

    remove some trash and add mikamore_

--- js/stream_info.js
+++ js/stream_info.js
@@ -1,10 +1,8 @@
 let streamers_arr = ["KoksyaChan", "k0kashechka", "aikasan_", "nodarbatman"]
 
-async function stream() {
-let i;
-
-    for (i = 0; i < streamers_arr.length; i++) {
-    $.get({url: `https://api.curly.team/twitch/?user=${streamers_arr[i]}`, async: false, success: function(result){
+$(async function(){
+    for (let i = 0; i < streamers_arr.length; i++) {
+    $.get({url: `http://api.curly.team/twitch/?user=${streamers_arr[i]}`, async: false, success: function(result){
     let data;
     if (result.length > 0) {
         data = `Live | ${result[0].title}`
@@ -14,9 +12,15 @@ let i;
     $(`.${streamers_arr[i]}`).text(data)
   }});
 };
-};
 
-$(async function(){
-    await stream()
+$.get({url: `http://api.curly.team/trovo/?user=mikamore_`, async: false, success: function(result){
+    let data;
+    if (result.is_live === true) {
+        data = `live | ${result.live_title}`
+    } else {
+        data = `Offline`
+    }
+    $(`.mikamore_`).text(data)
+}})
 });
 

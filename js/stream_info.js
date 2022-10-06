e42090e3a2e8df90f371ba08dadd52bd69f5886f
DexterLandry15 <56548223+DexterLandry15@users.noreply.github.com>
2022-10-06 18:32:23 +0300

    https

--- js/stream_info.js
+++ js/stream_info.js
@@ -2,7 +2,7 @@ let streamers_arr = ["KoksyaChan", "k0kashechka", "aikasan_", "nodarbatman"]
 
 $(async function(){
     for (let i = 0; i < streamers_arr.length; i++) {
-    $.get({url: `http://api.curly.team/twitch/?user=${streamers_arr[i]}`, async: false, success: function(result){
+    $.get({url: `https://api.curly.team/twitch/?user=${streamers_arr[i]}`, async: false, success: function(result){
     let data;
     if (result.length > 0) {
         data = `Live | ${result[0].title}`
@@ -13,7 +13,7 @@ $(async function(){
   }});
 };
 
-$.get({url: `http://api.curly.team/trovo/?user=mikamore_`, async: false, success: function(result){
+$.get({url: `https://api.curly.team/trovo/?user=mikamore_`, async: false, success: function(result){
     let data;
     if (result.is_live === true) {
         data = `live | ${result.live_title}`

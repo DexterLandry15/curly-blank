21a7bed12f20a6f88925bb3b0ffc34ea1908fd54
dexterlandry15 <dexter.landry15@gmail.com>
2022-10-13 23:15:39 +0300

    async

--- js/stream_info.js
+++ js/stream_info.js
@@ -2,7 +2,7 @@ let streamers_arr = ["KoksyaChan", "k0kashechka", "aikasan_", "nodarbatman"]
 $( document ).ready(function() {
 $(async function(){
     for (let i = 0; i < streamers_arr.length; i++) {
-    $.get({url: `https://api.curly.team/twitch/?user=${streamers_arr[i]}`, async: false, success: function(result){
+    $.get({url: `https://api.curly.team/twitch/?user=${streamers_arr[i]}`, async: true, success: function(result){
     let data;
     if (result.length > 0) {
         data = `Live | ${result[0].title}`
@@ -15,7 +15,7 @@ $(async function(){
 });
 
 $(async function(){
-    $.get({url: `https://api.curly.team/trovo/?user=mikamore_`, async: false, success: function(result){
+    $.get({url: `https://api.curly.team/trovo/?user=mikamore_`, async: true, success: function(result){
         let data;
         if (result.is_live === true) {
             data = `live | ${result.live_title}`

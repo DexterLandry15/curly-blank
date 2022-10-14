9f61a25176bf7c7c6c7cabc3a0c3821b0216bae5
dexterlandry15 <dexter.landry15@gmail.com>
2022-10-14 21:34:43 +0300

    lil animation

---
+++ js/dev.js
@@ -0,0 +1,17 @@
+let streamers_arr = ["KoksyaChan", "k0kashechka", "aikasan_", "nodarbatman"]
+$( document ).ready(function() {
+    $(async function(){
+        for (let i = 0; i < streamers_arr.length; i++) {
+
+            $(`.${streamers_arr[i]}`).removeClass("brightness-[0.4]").removeClass("animate-pulse");
+            $(`.${streamers_arr[i]}-live`).removeClass("hidden");
+
+      }});
+
+
+$(async function(){
+    $(`.mikamore_`).removeClass("brightness-[0.4]").removeClass("animate-pulse");
+    $(`.mikamore_-live`).removeClass("hidden");
+
+});
+});
